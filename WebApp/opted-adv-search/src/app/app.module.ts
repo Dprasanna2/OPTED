2db7f902940d5146456cc460c027770a48e9460f
PrasannaD <prasannadhamal1997@gmail.com>
2022-07-16 13:05:58 +0100

    restructure

--- WebApp/opted-adv-search/src/app/app.module.ts
+++ WebApp/opted-adv-search/src/app/app.module.ts
@@ -1,19 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AgGridModule } from '@ag-grid-community/angular';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RetryInterceptor } from './utilities/Interceptor/RetryInterceptor';
+import { TokenInterceptor } from './utilities/Interceptor/TokenInterceptor';
+import { SpinnerInterceptor } from './utilities/Interceptor/SpinnerInterceptor';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from './Services/Authentication.service';
+import { LoginPageComponent } from './z_components/common/login-page/login-page.component';
+import { SpinnerComponent } from './z_components/common/spinner/spinner.component';
+import { CanActivateGuard } from './utilities/Guard/can-activate.guard';
+import { AgGridModule } from '@ag-grid-community/angular';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    // RouterModule.forRoot(routes),
     AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AgGridModule
+    // NgbModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    SpinnerComponent,
+  ],
+  providers: [
+    // { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
+    // { provide: HTTP_INTERCEPTORS, useClass: RetryInterceptor, multi: true },
+    // { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    // AuthenticationService,
+    // CanActivateGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AgGridModule } from '@ag-grid-community/angular';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     AgGridModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
