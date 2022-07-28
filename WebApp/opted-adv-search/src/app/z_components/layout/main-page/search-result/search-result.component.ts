859e7c48bcd953f9e7abb417d1772803a6635461
shrikant katla <katlasrikant6@gmail.com>
2022-07-29 00:47:31 +0530

    Added search result html and pagination partial

--- WebApp/opted-adv-search/src/app/z_components/layout/main-page/search-result/search-result.component.ts
+++ WebApp/opted-adv-search/src/app/z_components/layout/main-page/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { searchResultPagination } from 'src/app/models/searchResult';
 
 @Component({
   selector: 'app-search-result',
@@ -6,24 +7,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.scss']
 })
 export class SearchResultComponent implements OnInit {
-
-  // modules = [ClientSideRowModelModule];
-  columnDefs = [
-        {headerName: 'Make', field: 'make', width: 150, resizable: true },
-        {headerName: 'Model', field: 'model', width: 150, resizable: true },
-        {headerName: 'Price', field: 'price', width: 150, resizable: true }
-    ];
-
-    rowData = [
-        {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
-    ];
+  resultData: any[] = [];
+  csvData: any[] = [];
+  csvHeaders: any[] = [];
+  countryList: Array<string> = [];
+  pagination: searchResultPagination = new searchResultPagination();
   constructor() { }
 
   ngOnInit() {
-
+    this.pagination.currentPageNo = 1;
+    this.pagination.pageSize = 20;
+    this.pagination.totalPages = 0;
+    this.getSearchDataList();
   }
 
+  getSearchDataList(){
+    const _csvData = localStorage.getItem('csvData');
+    const _csvHeaders = localStorage.getItem('csvHeaders');
+    this.csvData = _csvData ? JSON.parse(_csvData) : [];
+    console.log('this.csvData: ', this.csvData);
+    this.csvHeaders = _csvHeaders ? JSON.parse(_csvHeaders) : [];
+    if (this.csvData.length > 0) {
+      const totalPages = this.csvData.length / this.pagination.pageSize;
+      this.pagination.totalPages = Math.ceil(totalPages);
+      this.showData();
+    }
+  }
+
+  showData() {
+    const showDataLimit = this.pagination.currentPageNo * this.pagination.pageSize;
+    const initialData = showDataLimit - this.pagination.pageSize;
+    this.resultData = this.csvData.slice(initialData, showDataLimit);
+    this.setPaginationArray();
+  }
+
+  setPaginationArray() {
+    let array = this.pagination.paginationArray;
+    array = [];
+    const fisrtN0 = 1;
+    const currentNo = this.pagination.currentPageNo;
+    const LastNo = this.pagination.totalPages;
+    const arraySize = 5;
+    if(LastNo > 0) {
+      array.push(currentNo);
+      for (let i = 1; i < arraySize; i++) {
+        const preNum = array[0] - 1;
+        if(preNum > 0) {
+          array.unshift(preNum);
+        } 
+        
+        const postNum = array[array.length - 1] + 1;
+        if(postNum <= LastNo) {
+          array.push(postNum);
+        }
+
+        if (array.length === arraySize) {
+          break;
+        }
+      }
+
+      // let itemNeedToInsert = Math.floor(arraySize/2);
+      // for (let i = 0; i < preArraySize; i++) {
+      //   const num = array[0] - 1;
+      //   if(num > 0) {
+      //     array.unshift(num);
+      //   } else {
+      //     break;
+      //   }
+      // }
+
+      // let postArraySize = preArraySize + (preArraySize - (array.length - 1));
+      // for (let i = 0; i < postArraySize; i++) {
+      //   const num = array[array.length - 1] + 1;
+      //   if(num < LastNo) {
+      //     array.push(num);
+      //   } else {
+      //     break;
+      //   }
+      // }
+
+      this.pagination.paginationArray = array;
+      this.pagination.showFirstIcon = array.length === arraySize && array[0] !== 1;
+      this.pagination.showLastIcon = array.length === arraySize && array[array.length - 1] !== LastNo;
+    }
+  }
+
+  changePage(num: number) {
+    this.pagination.currentPageNo = num;
+    this.showData();
+  }
 }
  
